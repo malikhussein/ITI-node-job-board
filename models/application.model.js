7c1bce99d179b8a1b63d83b93c79d411e3839e70
Malik Hussein <malikhussein229@gmail.com>
2025-02-26 04:06:10 +0200

    Updated application schema Added a job field with relation to Job Added a applicant field with relation to User Added a resume field with relation to Resume Updated the status field with emun

--- models/application.model.js
+++ models/application.model.js
@@ -2,10 +2,26 @@ import { model, Schema } from 'mongoose';
 
 const applicationSchema = new Schema(
   {
-    // User Rel
-    // Job Rel
+    job: {
+      type: Schema.Types.ObjectId,
+      ref: 'Job',
+      required: true,
+    },
+    applicant: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    // ? Will the resume be required to apply to job
+    resume: {
+      type: Schema.Types.ObjectId,
+      ref: 'Resume',
+      required: true,
+    },
     status: {
       type: String,
+      enum: ['pending', 'reviewed', 'accepted', 'rejected'],
+      default: 'pending',
     },
   },
   { timestamps: true }
