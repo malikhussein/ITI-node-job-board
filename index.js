13826149c4c9222f276af446d5c20fd83d7ac51d
Test0-VC <omarabdin1@gmail.com>
2025-02-26 19:43:33 +0200

    Added 404 route for any wrong apis + Added app.use(express.json())

--- index.js
+++ index.js
@@ -8,12 +8,16 @@ connectDB();
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('Server working');
 });
 
 app.use('/api', mainRouter);
-
+app.all('*', (req, res) => {
+  return res.status(404).send('API route not found');
+});
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
 });
