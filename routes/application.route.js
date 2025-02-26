37c45ce835fabebe33af0704fe7d19f6686ac859
Malik Hussein <malikhussein229@gmail.com>
2025-02-26 04:15:04 +0200

    Created a route file to handle the routes for the application

---
+++ routes/application.route.js
@@ -0,0 +1,8 @@
+import { Router } from 'express';
+import Application from '../controllers/application.controller.js';
+
+const applicationRouter = Router();
+
+applicationRouter.post('/:jobId', Application.applyJob);
+
+export default applicationRouter;
