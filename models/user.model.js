e0ee6c3c172e77fdb6d3936bd4ee7e214d6feda7
Test0-VC <omarabdin1@gmail.com>
2025-02-26 18:22:17 +0200

    Adjusted User Schema

--- models/user.model.js
+++ models/user.model.js
@@ -1,20 +1,44 @@
 import { model, Schema } from 'mongoose';
+const roleType = {
+  job_seeker: 'job_seeker',
+  employer: 'employer',
+  Admin: 'Admin',
+};
 
 const userSchema = new Schema(
   {
-    name: {
+    userName: {
       type: String,
+      required: [true, 'UserName field is required'],
+      minlength: 2,
+      maxlength: 15,
     },
     email: {
       type: String,
+      required: [true, 'Email is required'],
+      unique: true,
+
+    },
+    confirmEmail: {
+      type: Boolean,
+      default: false,
+    },
+  
+    //TODO
+    image :{
+      type : String,
+      default: "https://static.vecteezy.com/system/resources/previews/026/619/142/non_2x/default-avatar-profile-icon-of-social-media-user-photo-image-vector.jpg" ,
+      required: false,
     },
     password: {
       type: String,
+      required: [true, 'Password is required'],
+
     },
     role: {
       type: String,
-      enum: ['admin', 'job-seeker', 'employer'],
-      default: 'job-seeker',
+      enum: Object.values(roleType), // Retrieve all values from the roleType object
+      default: roleType.job_seeker,
     },
   },
   { timestamps: true }
