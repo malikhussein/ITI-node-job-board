dac7666f85154c6cb1fe0358826171b6a4a06815
Malik Hussein <malikhussein229@gmail.com>
2025-02-28 15:47:39 +0200

    added the auth middleware

--- routes/application.route.js
+++ routes/application.route.js
@@ -1,13 +1,18 @@
 import { Router } from 'express';
 import Application from '../controllers/application.controller.js';
+import authMiddleware from '../middleware/auth.middleware.js';
 
 const applicationRouter = Router();
 
-applicationRouter.post('/:jobId', Application.applyJob);
-applicationRouter.get('/', Application.getUserApplications);
-applicationRouter.get('/:jobId', Application.getJobApplications);
+applicationRouter.post('/:jobId', authMiddleware, Application.applyJob);
+applicationRouter.get('/', authMiddleware, Application.getUserApplications);
+applicationRouter.get(
+  '/:jobId',
+  authMiddleware,
+  Application.getJobApplications
+);
 // ? /:id/status or /status/:id
-applicationRouter.put('/:id/status', Application.updateStatus);
-applicationRouter.delete('/:id', Application.deleteApplication);
+applicationRouter.put('/:id/status', authMiddleware, Application.updateStatus);
+applicationRouter.delete('/:id', authMiddleware, Application.deleteApplication);
 
 export default applicationRouter;
